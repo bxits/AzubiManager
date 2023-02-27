fd402ed7ca54c924afe4b3bf56f9c7aaa1024d9f
bxits <71964606+bxits@users.noreply.github.com>
2023-02-27 09:39:24 +0100

    Initial commit

---
+++ DataAccess/DataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DataAccess
+    {
+        public bool SaveAzubi(Azubi a)
+        {
+
+            return true;
+        }
+    }
+}
